c41dc6b73033dca123c0f9c2fca2ac97c80ecbaa
Sanvi <91210918+sanvi01@users.noreply.github.com>
2024-05-02 00:08:59 +0530

    Create codefile.py

---
+++ Password Generator v2/codefile.py
@@ -0,0 +1,29 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+import random
+print("Welcom to pyPasword creator!")
+password = []
+letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+numbers = ['1','2','3','4','5','6','7','8','9','0']
+symbols = ['!','@','#','$','%','^','&','*','(',')','+','-']
+
+no_letters = int(input("How many letters do you want in you password?\n"))
+no_nums = int(input("How many numbers do you want in your password?\n"))
+no_symbols = int(input("How many symbols do you want in your password?\n"))
+
+
+for i in range(0, no_letters):
+    password.insert(random.randint(0,26),random.choice(letters))
+  
+for i in range(0, no_nums):
+    password.insert(random.randint(0,10),random.choice(numbers))
+
+for i in range(0, no_symbols):
+    password.insert(random.randint(0,12),random.choice(symbols))
+
+final_pass = ""    
+for i in password:
+    final_pass += i
+
+print(f"Your password is: {final_pass}")
+    
